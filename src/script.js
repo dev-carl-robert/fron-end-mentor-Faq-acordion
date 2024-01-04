1dde8552058ebc44c4321ef81e45fb8dc690bd64
Carlos Robert <149803558+dev-carl-robert@users.noreply.github.com>
2024-01-03 21:29:16 -0300

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,12 +1,18 @@
 const askActived = document.querySelectorAll(".asks__content");
-const description = document.querySelectorAll(".ask__descriptions");
 
-
-askActived.forEach(function (asks__content, ask__descriptions) {
+askActived.forEach(function (asks__content) {
     asks__content.addEventListener("click", function () {
-        asks__content.classList.toggle("actived")
+        asks__content.classList.toggle("actived");
 
-        let img = document.querySelector("#image");
-        img.setAttribute('src', './src/assets/images/icon-minus.svg');
-    })
-})
+        let img = asks__content.querySelector(".toggle-icon");
+
+        // Verifica se a classe "actived" está presente no elemento
+        if (asks__content.classList.contains("actived")) {
+            // Se estiver presente, troca para o ícone de fechar
+            img.setAttribute('src', './src/assets/images/icon-minus.svg');
+        } else {
+            // Caso contrário, troca para o ícone de abrir
+            img.setAttribute('src', './src/assets/images/icon-plus.svg');
+        }
+    });
+});
